bbefeb7237ec716307568ea855f2d940562af9c9
Ricardo Duarte <rickduarte@gmail.com>
2023-04-03 15:13:34 -0300

    add responsive design

--- flutter_ui_widgets/lib/widgets/article_card.dart
+++ flutter_ui_widgets/lib/widgets/article_card.dart
@@ -1,28 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_widgets/models/article.dart';
+import '../models/article.dart';
 
 class ArticleCard extends StatelessWidget {
   final Article article;
 
-  const ArticleCard({super.key, required this.article});
+  const ArticleCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        margin: const EdgeInsets.all(16.0),
-        elevation: 4,
-        child: Column(
-          children: [
-            CardBanner(imageUrl: article.urlToImage),
-            CardDetail(article: article),
-          ],
-        ));
+      margin: const EdgeInsets.all(16),
+      elevation: 4,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final isTablet = constraints.maxWidth > 600;
+          return isTablet
+              ? WideCard(article: article)
+              : TallCard(article: article);
+        },
+      ),
+    );
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   final isPortrait =
+  //       MediaQuery.of(context).orientation == Orientation.portrait;
+  //   return Card(
+  //     margin: const EdgeInsets.all(16),
+  //     elevation: 4,
+  //     child:
+  //         isPortrait
+  //          ? TallCard(article: article)
+  //          : WideCard(article: article),
+  //   );
+  // }
+}
+
+class TallCard extends StatelessWidget {
+  const TallCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CardBanner(imageUrl: article.urlToImage),
+        CardDetail(article: article),
+      ],
+    );
+  }
+}
+
+class WideCard extends StatelessWidget {
+  const WideCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CardBanner(
+          imageUrl: article.urlToImage,
+          isWideCard: true,
+        ),
+        Expanded(
+          child: CardDetail(article: article),
+        ),
+      ],
+    );
   }
 }
 
 class CardBanner extends StatelessWidget {
   final String? imageUrl;
-  const CardBanner({Key? key, this.imageUrl}) : super(key: key);
+  final bool isWideCard;
+
+  const CardBanner({
+    Key? key,
+    this.imageUrl,
+    this.isWideCard = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,24 +98,25 @@ class CardBanner extends StatelessWidget {
       children: [
         ClipRRect(
           borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(4.0),
-            topRight: Radius.circular(4.0),
+            topLeft: Radius.circular(4),
+            topRight: Radius.circular(4),
           ),
           child: SizedBox(
-              height: 200,
-              width: double.infinity,
-              child: Image.network(
-                imageUrl!,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, progress) {
-                  if (progress == null) return child;
-                  return const Center(child: CircularProgressIndicator());
-                },
-              )),
+            height: isWideCard ? 150 : 200,
+            width: isWideCard ? 150 : double.infinity,
+            child: Image.network(
+              imageUrl!,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, progress) {
+                if (progress == null) return child;
+                return const Center(child: CircularProgressIndicator());
+              },
+            ),
+          ),
         ),
         Positioned(
-          top: 10,
-          right: 10,
+          top: isWideCard ? 2 : 10,
+          right: isWideCard ? 2 : 10,
           child: IconButton(
             icon: const Icon(Icons.bookmark_border, size: 32),
             onPressed: () {},
@@ -65,18 +134,25 @@ class CardDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    return Container(
+      height: isPortrait ? 130 : 150,
       padding: const EdgeInsets.all(16),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text(
             article!.title,
-            style: Theme.of(context).textTheme.headlineSmall,
+            // style: const TextStyle(fontSize: 24),
+            // style: Theme.of(context).textTheme.headline5!.copyWith(
+            //       fontStyle: FontStyle.italic,
+            //       color: Colors.grey[600],
+            //     ),
+            style: Theme.of(context).textTheme.headline5,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
           ),
-          const SizedBox(height: 16),
           Row(
             children: <Widget>[
               Text(article!.source),
